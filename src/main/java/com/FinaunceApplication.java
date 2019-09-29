bfa22313de1210e804601fd36c5a3621c3df0819
wcy <792053282@qq.com>
2019-09-29 21:02:01 +0800

    项目启动

---
+++ src/main/java/com/FinaunceApplication.java
@@ -0,0 +1,16 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author
+ * @date 2019/9/29
+ * @Time 20:56
+ */
+@SpringBootApplication
+public class FinaunceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FinaunceApplication.class,args);
+    }
+}
