ff2d8adda7e9b72238875a0fe3bb174ba9b8a9cc
hxh <1607706432@qq.com>
2019-10-09 14:03:47 +0800

    何晓怀修改

---
+++ src/main/java/com/common/LoginType.java
@@ -0,0 +1,23 @@
+package com.common;
+
+/**
+ * @author HeXiaoH
+ * @date 2019/10/9 11:05
+ */
+public enum  LoginType {
+    USER("user"),
+    ADMIN("admin");
+
+    private String type;
+
+    private LoginType(String type){
+        this.type=type;
+    }
+
+    @Override
+    public String toString() {
+        return this.type.toString();
+    }
+
+
+}
